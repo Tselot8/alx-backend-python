50a6c05f499117026eeed9692860e1a06e480468
Tselot Million <tselotmillion8@gmail.com>
2025-09-07 21:30:45 +0300

    Missing features

--- python-generators-0x00/1-batch_processing.py
+++ python-generators-0x00/1-batch_processing.py
@@ -10,7 +10,7 @@ def stream_users_in_batches(batch_size):
     connection = psycopg2.connect(
         host="localhost",
         user="postgres",         # replace with your DB user
-        password="yourpassword", # replace with your DB password
+        password="Myart@2023!", # replace with your DB password
         database="ALX_prodev"
     )
     cursor = connection.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
@@ -20,8 +20,7 @@ def stream_users_in_batches(batch_size):
         rows = cursor.fetchmany(batch_size)
         if not rows:
             break
-        yield rows  # yields a batch of rows
-
+        yield rows
     cursor.close()
     connection.close()
 
@@ -31,7 +30,7 @@ def batch_processing(batch_size):
     """
     Processes each batch of users, filtering those over age 25
     """
-    for batch in stream_users_in_batches(batch_size):  # loop 1
-        for user in batch:                              # loop 2
+    for batch in stream_users_in_batches(batch_size):
+        for user in batch:
             if user['age'] > 25:
-                yield user                              # yield filtered users
+                yield user  
