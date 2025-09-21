62b70a34df5c536175ee7707727d18c1294b2a91
Tselot Million <tselotmillion8@gmail.com>
2025-09-21 21:22:44 +0300

    messaging_app

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import ConversationViewSet, MessageViewSet
 
-# Create a DRF router and register our viewsets
+# Create a DRF default router
 router = DefaultRouter()
 router.register(r'conversations', ConversationViewSet, basename='conversation')
 router.register(r'messages', MessageViewSet, basename='message')
--- messaging_app/messaging_app/urls.py
+++ messaging_app/messaging_app/urls.py
@@ -4,5 +4,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include('chats.urls')),  # Add this line
+    path('api/', include('chats.urls')),       # include your chats API endpoints
+    path('api-auth/', include('rest_framework.urls')),  # DRF login for browsable API
 ]
