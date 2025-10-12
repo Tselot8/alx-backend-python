fd7b04588fef9203e83660df58d4ae33b66df7bd
Tselot Million <tselotmillion8@gmail.com>
2025-10-12 15:18:05 +0300

    Multi-Container Setup

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -83,10 +83,10 @@ WSGI_APPLICATION = 'messaging_app.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv("POSTGRES_DB", "chatdb"),
-        'USER': os.getenv("POSTGRES_USER", "chatuser"),
-        'PASSWORD': os.getenv("POSTGRES_PASSWORD", "chatpass"),
-        'HOST': 'db',  # matches the service name in docker-compose
+        'NAME': os.getenv('MYSQL_DB', 'chatdb'),
+        'USER': os.getenv('MYSQL_USER', 'chatuser'),
+        'PASSWORD': os.getenv('MYSQL_PASSWORD', 'chatpass'),
+        'HOST': os.getenv('MYSQL_HOST', 'db'),
         'PORT': '5432',
     }
 }
